2f030066d669cc08e1ddcee453ff2926d2c9ee2d
hosei0514 <44825424+hosei0514@users.noreply.github.com>
2021-09-05 20:39:53 +0900

    Update TodoController.php

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,91 +1,69 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/index.css')}}">
 
-namespace App\Http\Controllers;
+</head>
+<body>
+    <div class=todo>
+    <div class="container" style="margin:400px 100px;">
+    <div class="container" style="width:1000px;">
+    <div class="container mt-3">
+        <h1>Todo List</h1>
+    </div>
+    <div class="container mt-3">
+        <div class="container mb-4">
+            {!! Form::open(['route' => 'todos.store', 'method' => 'POST']) !!}
+            {{ csrf_field() }}
+                <div class="row">
+                    {{ Form::text('newTodo', null, ['class' => 'form-control col-8 mr-5']) }}
+                    {{ Form::submit('追加', ['class' => 'btn btn-primary']) }}
+                </div>
+            {!! Form::close() !!}
+        </div>
+        @if ($errors->has('newTodo'))
+            <p class="alert alert-danger">{{ $errors->first('newTodo') }}</p>
+        @endif
 
-use Illuminate\Http\Request;
-use App\Todo;
-use Brian2694\Toastr\Facades\Toastr;
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" style="width: 60%">タスク名</th>
+                    <th scope="col">作成日</th>
+                    <th scope="col">更新</th>
+                    <th scope="col">削除</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($todos as $todo)
+                    <tr>
+                        <td>
+                        {{ Form::text('updateTodo', $todo->todo, ['class' => 'form-control col-7 mr-4']) }}
+                        </td>
+                        <td>{{ $todo->created_at }}</td>
+                        {!! Form::open(['route' => ['todos.update', $todo->id], 'method' => 'POST']) !!}
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+                        <input type="hidden" class="input-update" value="{{ $todo->todo }}" name="todo" />
+                        <td>{{ Form::submit('更新', ['class' => 'btn btn-danger']) }}</td>
+                        {!! Form::close() !!}
+                        {!! Form::open(['route' => ['todos.destroy', $todo->id], 'method' => 'POST']) !!}
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <td>{{ Form::submit('削除', ['class' => 'btn btn-danger']) }}</td>
+                        {!! Form::close() !!}
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    </div>
 
-class TodoController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $todos = Todo::orderByRaw('`deadline` IS NULL ASC')->get();
-
-        return view('todos.index', [
-            'todos' => $todos,
-        ]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'newTodo'     => 'required|max:20',
-        ]);
-
-        Todo::create([
-            'todo'     => $request->newTodo,
-        ]);
-
-        return redirect()->route('todos.index');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $todo = Todo::find($id);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'updateTodo'     => 'required|max:20',
-        ]);
-
-        $todo = Todo::find($id);
-
-        $todo->todo     = $request->updateTodo;
-
-        $todo->save();
-
-        return redirect()->route('todos.index');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $todo = Todo::find($id);
-
-        $todo->delete();
-
-        return redirect()->route('todos.index');
-    }
-}
+    <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
+</body>
+</html>
